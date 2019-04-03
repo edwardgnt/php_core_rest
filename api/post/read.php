f0564d4e62ab3b417bf3cb5ba76a7459385fa183
edwardgnt <edwardgnt@gmail.com>
2019-04-02 21:49:24 -0700

    First commit

---
+++ api/post/read.php
@@ -0,0 +1,46 @@
+<?php
+
+// Headers
+header('Access-Control-Allow-Origin: *');
+header('Content-Type: application-json');
+
+include_once '../../config/Database.php';
+include_once '../../models/Post.php';
+
+// Instantiate Database
+$database = new Database();
+$db = $database->connect();
+
+// Instantiate Blog Post
+$post = new Post($db);
+
+// Blog post query
+$result = $post->read();
+
+$num = $result->rowCount();
+
+// Check if posts
+if($num > 0) {
+    $posts_arr = array();
+    $posts_arr['data'] = array();
+
+    while($row = $result->fetch(PDO::FETCH_ASSOC)) {
+
+        $post_item = array(
+            'id' => $row['id'],
+            'title' => $row['title'],
+            'body' => html_entity_decode($row['body']),
+            'author' => $row['author'],
+            'category_id' => $row['category_id'],
+            'category_name' => $row['category_name']
+        );
+
+        // Push to "data" array
+        array_push($posts_arr['data'], $post_item);
+    }
+
+    // Turn to JSON & output
+    echo json_encode($posts_arr);
+} else {
+    echo json_encode('No Posts Found');
+}
---
+++ config/Database.php
@@ -0,0 +1,26 @@
+<?php
+
+class Database {
+    // DB Params
+    private $host = 'localhost';
+    private $db_name = 'myblog';
+    private $username = 'root';
+    private $password = '!Jarvis2019';
+    private $conn;
+
+    // DB connect
+    public function connect()
+    {
+        $this->conn = null;
+
+        try {
+            $this->conn = new PDO('mysql:host=' . $this->host . ';dbname=' . $this->db_name, $this->username, $this->password);
+            $this->conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        } catch(PDOException $e) {
+            echo 'Error connecting: ' . $e->getMessage();
+        }
+
+        return $this->conn;
+    }
+
+}
